5e797646ae2e5629fe07d2979b232bb69ea055b1
jjjosueva <alexanderjosue192004@gmail.com>
2024-11-29 09:01:50 -0500

    actualizacion de los apartados

--- examenjosuevalencia/GridLayout.xaml.cs
+++ examenjosuevalencia/GridLayout.xaml.cs
@@ -6,5 +6,19 @@ namespace examenjosuevalencia
         {
             InitializeComponent();
         }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            string firstName = FirstNameEntry.Text;
+            string secondName = SecondNameEntry.Text;
+
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(secondName))
+            {
+                await DisplayAlert("Error", "Por favor, ingrese todos los datos.", "OK");
+                return;
+            }
+
+            await DisplayAlert("Datos Guardados", $"Primer Nombre: {firstName}\nSegundo Nombre: {secondName}", "OK");
+        }
     }
 }

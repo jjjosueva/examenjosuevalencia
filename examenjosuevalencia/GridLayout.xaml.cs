e4bad9e3a2a196fcdf5ecff37cade384de00156d
jjjosueva <alexanderjosue192004@gmail.com>
2024-11-29 09:59:04 -0500

    modifique los paginas y  aparte de programacion para que cumpla los requerimeintos

--- examenjosuevalencia/GridLayout.xaml.cs
+++ examenjosuevalencia/GridLayout.xaml.cs
@@ -1,24 +1,9 @@
-namespace examenjosuevalencia
+namespace examenjosuevalencia;
+
+public partial class GridLayout : ContentPage
 {
-    public partial class GridLayout : ContentPage
+    public GridLayout()
     {
-        public GridLayout()
-        {
-            InitializeComponent();
-        }
-
-        private async void SaveButton_Clicked(object sender, EventArgs e)
-        {
-            string firstName = FirstNameEntry.Text;
-            string secondName = SecondNameEntry.Text;
-
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(secondName))
-            {
-                await DisplayAlert("Error", "Por favor, ingrese todos los datos.", "OK");
-                return;
-            }
-
-            await DisplayAlert("Datos Guardados", $"Primer Nombre: {firstName}\nSegundo Nombre: {secondName}", "OK");
-        }
+        InitializeComponent();
     }
 }

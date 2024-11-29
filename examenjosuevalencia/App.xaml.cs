725ccafda8c48022a5fdab3d17b904af830dfc69
laboratorio <laboratorio@UPOAULA10622>
2024-11-29 07:13:00 -0500

    Agregar archivos de proyecto.

---
+++ examenjosuevalencia/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace examenjosuevalencia
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ examenjosuevalencia/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace examenjosuevalencia
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
